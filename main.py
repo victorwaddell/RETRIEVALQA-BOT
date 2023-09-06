8e3917535f3bd965140eb534330432af803ead92
Victor Waddell <victor.waddell@gmail.com>
2023-09-05 22:25:16 -0400

    broken but pushed for later changes

--- main.py
+++ main.py
@@ -85,15 +85,66 @@ conversational_agent = initialize_conversational_agent(agent, create_tools(retri
 # MAIN CHATBOT LOOP
 
 from HelperFunctions.mainhelper import get_query
+import gradio as gr
 
-if __name__ == "__main__":  # Main prompt loop
-    while True:
-        query = get_query()
-        if query.lower() in ['quit', 'q', 'exit']:
-            break
-        print("Vectorstore Chunks: ", pineconeretrieval(pinecone_vectordb, query, "mmr"),"\n\n\n")
-        answer = "Agent Result: " + conversational_agent.run(query)
-        if answer:
-            print(answer)
+title = """
+<div style="text-align: center;max-width: 700px;">
+    <h1>Chat with PDF</h1>
+    <p style="text-align: center;">Upload a .PDF from your computer, click the "Load PDF to LangChain" button, <br />
+    when everything is ready, you can start asking questions about the pdf ;)</p>
+    <a style="display:inline-block; margin-left: 1em" href="https://huggingface.co/spaces/fffiloni/langchain-chat-with-pdf?duplicate=true"><img src="https://img.shields.io/badge/-Duplicate%20Space%20to%20skip%20the%20queue-blue?labelColor=white&style=flat&logo=data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAP5JREFUOE+lk7FqAkEURY+ltunEgFXS2sZGIbXfEPdLlnxJyDdYB62sbbUKpLbVNhyYFzbrrA74YJlh9r079973psed0cvUD4A+4HoCjsA85X0Dfn/RBLBgBDxnQPfAEJgBY+A9gALA4tcbamSzS4xq4FOQAJgCDwV2CPKV8tZAJcAjMMkUe1vX+U+SMhfAJEHasQIWmXNN3abzDwHUrgcRGmYcgKe0bxrblHEB4E/pndMazNpSZGcsZdBlYJcEL9Afo75molJyM2FxmPgmgPqlWNLGfwZGG6UiyEvLzHYDmoPkDDiNm9JR9uboiONcBXrpY1qmgs21x1QwyZcpvxt9NS09PlsPAAAAAElFTkSuQmCC&logoWidth=14" alt="Duplicate Space"></a>
+</div>
+"""
+
+def bot(query):
+    print("Debug: Entering bot function")
+
+    if not query:
+        print("Debug: query is None or empty!")
+        return "Please enter a valid query."
+
+    print(f"Debug: Starting retrieval for query: {query}")
+    retrieval_result = pineconeretrieval(pinecone_vectordb, query, "mmr")
+    print("Debug: Retrieval completed")
+
+    print("Debug: Starting conversational agent")
+    agent_result = conversational_agent.run(query)
+    print("Debug: Conversational agent completed")
+
+    return f"Vectorstore Chunks: {retrieval_result}\nAgent Result: {agent_result}"
+
+
+
+def update_chatbot(query=None):
+    print("Debug: update_chatbot triggered")  # Add this line
+    global chat_history  # Use the global chat_history list
+    if query is None:
+        print("Debug: query is None")
+    else:
+        print(f"Debug: query is {query}")
+
+    if query:
+        print("Debug: About to call bot function")
+        chat_response = bot(query)
+        print("Debug: Bot function returned")
+        print(f"Debug: Chat response is {chat_response}")
+
+        # Your chatbot update logic
+        print("Debug: About to update chatbot")
+        chatbot.set_message([
+            (entry["message"], entry["role"]) for entry in chat_history])
+        print("Debug: Chatbot updated")
+
+# Gradio Interface
+with gr.Blocks() as app:
+    chatbot = gr.Chatbot([], height=350)
+    question = gr.Textbox(label="Question", placeholder="Type your question and hit Enter")
+    submit_btn = gr.Button("Send message")
+
+    # Link the submit and click actions to the update_chatbot function
+    question.submit(update_chatbot)
+    submit_btn.click(update_chatbot)
+
+app.launch()
 
 index.delete(index_name)  # Delete index
